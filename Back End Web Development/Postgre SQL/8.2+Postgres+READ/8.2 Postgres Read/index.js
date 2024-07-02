49aed43356f7592691c2eac9a0d507f006f4217e
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-02 11:49:10 +0530

    Web Development Codes and Projects

--- Back End Web Development/Postgre SQL/8.2+Postgres+READ/8.2 Postgres Read/index.js
+++ Back End Web Development/Postgre SQL/8.2+Postgres+READ/8.2 Postgres Read/index.js
@@ -21,4 +21,49 @@ app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
+// quix strores data retrived from flag.csv so it can be used
+let quiz = [];
+db.connect();
 
+db.query("SELECT * FROM flag",(err,res) => {
+  if(err){
+    console.error("There has been an error in fetching the data",error);
+  }else{
+    quiz = res.rows;
+  }
+  db.end();
+});
+
+let curr_question = {};
+let TotalScore = 0;
+
+
+app.get("/",async (req,res) => {
+  TotalScore = 0;
+  await nextQuestion();
+  res.render("index.ejs",{
+    question : curr_question
+  });
+});
+
+
+app.post("/submit",(req,res) => {
+  let answer = req.body.answer.trim();
+  let IsCorrect = false;
+  if(curr_question.name.toLowerCase() == answer.toLowerCase()){
+    TotalScore += 1;
+    IsCorrect = true;
+  }
+  nextQuestion();
+
+  res.render("index.ejs",{
+    question:curr_question,
+    wasCorrect : IsCorrect,
+    totalScore : TotalScore 
+  })
+})
+
+async function nextQuestion(){
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  curr_question = randomCountry;
+}
