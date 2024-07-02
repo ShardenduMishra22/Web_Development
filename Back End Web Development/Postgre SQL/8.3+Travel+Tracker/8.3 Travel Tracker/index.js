4c26e610382d1d0bda077a071bd5d3a837f24ce6
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-02 16:16:29 +0530

    Web Development Codes and Projects

--- Back End Web Development/Postgre SQL/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
+++ Back End Web Development/Postgre SQL/8.3+Travel+Tracker/8.3 Travel Tracker/index.js
@@ -1,16 +1,37 @@
-import express from "express";
 import bodyParser from "body-parser";
+import express from "express";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended : true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+})
+
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "world",
+  password : "Iamshardendumishra@2244",
+  port : "5432"
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+db.connect();
+
+app.get("/",async (req,res) => {
+  const result = await db.query("SELECT country_code FROM  visited_country");
+  let countries = [];
+  result.rows.forEach( (i) => {
+    countries.push(i.country_code);
+  });  
+  console.log(result.rows);
+  res.render("index.ejs", {
+    countries : countries,
+    total : countries.length,
+  });
+  db.end();
 });
