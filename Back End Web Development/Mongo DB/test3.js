76f9dab64e03ae389c76288e1cf0a5219735f7ed
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-05 23:41:54 +0530

    Web Development Codes and Projects

--- Back End Web Development/Mongo DB/test3.js
+++ Back End Web Development/Mongo DB/test3.js
@@ -1,11 +1,14 @@
-// Find books where the price is greater than 12
-db.books.find({$expr: { $gt: ["$price", 12] }});
+// // Find books where the price is greater than 12
+// db.Table_3.find({$expr: { $gt: ["$price", 12] }});
 
-// Find books where the _id is divisible by 3
-db.books.find({ _id: { $mod: [3, 0] } });
+// // Find books where the _id is divisible by 3
+// db.Table_3.find({ _id: { $mod: [3, 0] } });
 
-// Find books where the title contains "Potter"
-db.books.find({ title: { $regex: /Potter/ } });
+// // Find books where the title contains "Potter"
+// db.Table_3.find({ title: { $regex: /Potter/ } });
 
-// Perform full-text search for books containing "New" in the title
-db.books.find({ $text: { $search: "New" } });
+// // Perform full-text search for books containing "New" in the title
+// db.Table_3.find({ $text: { $search: "New" } });
+
+// // index
+// db.collection.createIndex({ "name": "text" })
